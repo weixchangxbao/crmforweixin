0b52f6255ae2cc08495b00c497b846041960b3d4
dolica <wangchen3224@gmail.com>
2013-11-19 13:32:11 +0800

    init commit

---
+++ src/main/java/com/brother/basic/entity/User.java
@@ -0,0 +1,193 @@
+package com.brother.basic.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="BT_User")
+public class User extends IdEntity{
+	
+	private String username;
+	
+	private String realname;
+
+	private String telephone;
+
+	private String mobilephone;
+
+	private String department;
+
+	private String title;
+
+	private String headportrait;
+
+	@Temporal(TemporalType.TIME)
+	private Date createTime;
+
+	private String passwd;
+
+	private String comment;
+
+	@Temporal(TemporalType.DATE)
+	private Date expiredDate;
+
+	private int active;
+
+	private int loginTimes;
+
+	private String email;
+
+	@Temporal(TemporalType.TIME)
+	private Date lastLoginTime;
+
+	private String lastLoginIP;
+	
+	@ManyToOne(targetEntity=Role.class)
+	@JoinTable(name="BT_RoleRelUser",joinColumns={@JoinColumn(name="BT_User_ID")},
+	inverseJoinColumns={@JoinColumn(name="BT_Role_ID")})
+	private Role role;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getRealname() {
+		return realname;
+	}
+
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getMobilephone() {
+		return mobilephone;
+	}
+
+	public void setMobilephone(String mobilephone) {
+		this.mobilephone = mobilephone;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getHeadportrait() {
+		return headportrait;
+	}
+
+	public void setHeadportrait(String headportrait) {
+		this.headportrait = headportrait;
+	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public Date getExpiredDate() {
+		return expiredDate;
+	}
+
+	public void setExpiredDate(Date expiredDate) {
+		this.expiredDate = expiredDate;
+	}
+
+	public int getActive() {
+		return active;
+	}
+
+	public void setActive(int active) {
+		this.active = active;
+	}
+
+	public int getLoginTimes() {
+		return loginTimes;
+	}
+
+	public void setLoginTimes(int loginTimes) {
+		this.loginTimes = loginTimes;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getLastLoginTime() {
+		return lastLoginTime;
+	}
+
+	public void setLastLoginTime(Date lastLoginTime) {
+		this.lastLoginTime = lastLoginTime;
+	}
+
+	public String getLastLoginIP() {
+		return lastLoginIP;
+	}
+
+	public void setLastLoginIP(String lastLoginIP) {
+		this.lastLoginIP = lastLoginIP;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+	
+}
