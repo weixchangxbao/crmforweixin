f053fd60c1eb02de4a315ffc0229e045973bc125
dolica <wangchen3224@gmail.com>
2013-12-02 12:46:10 +0800

    提交oracle初始化sql

--- src/main/resources/sql/mysql/user.sql
+++ src/main/resources/sql/mysql/user.sql
@@ -4,4 +4,4 @@ LIMIT 0, 1000
 
 -- Date: 2013-11-30 21:35
 */
-INSERT INTO `bt_user` (`id`,`create_time`,`department`,`last_loginip`,`last_login_time`,`login_times`,`mobilephone`,`password`,`realname`,`salt`,`telephone`,`username`,`rolename`) VALUES (1,'2001-00-00',NULL,NULL,NULL,0,NULL,'691b14d79bf0fa2215f155235df5e670b64394cc','Admin','7efbd59d9741d34f',NULL,'admin',NULL);
+INSERT INTO bt_user (id,create_time,department,mobilephone,password,realname,salt,telephone,username,rolename) VALUES (1,sysdate,NULL,NULL,'691b14d79bf0fa2215f155235df5e670b64394cc','Admin','7efbd59d9741d34f',NULL,'admin','超级管理员');
